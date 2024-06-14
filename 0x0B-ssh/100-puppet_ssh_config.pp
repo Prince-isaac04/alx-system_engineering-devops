8d67552983f3a5e0bbb52e7fd1ac68b56091fc8e
Prince-isaac04 <ajayioluwaseyi1692002@gmail.com>
2024-06-13 22:12:01 -0700

    add

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,13 @@
- server Execute a command
-
-exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
-        path    => '/bin/'
+{ '/root/.ssh/config':
+  ensure  => file,
+  mode    => '0600',
+  owner   => 'root',
+  group   => 'root',
+  content => @("EOF")
+  Host 	52.87.215.247
+      IdentityFile ~/.ssh/school
+      PreferredAuthentications publickey
+      PasswordAuthentication no
+  | EOF
 }
+
