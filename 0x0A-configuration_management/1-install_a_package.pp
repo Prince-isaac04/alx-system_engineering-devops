fd6eae61c83f2a01d225c34e1765ef28a5a431cc
Prince_isaac04 <ajayioluwaseyi1692002@gmail.com>
2024-07-14 16:58:27 -0700

    execute command

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,11 @@
 #installing flask using pippet
-package { 'flask':
-  ensure   => '2.1',
-  provider => 'pip3',
-}
 package { 'python3.8':
   ensure   => '3.8.10',
+  provider => 'pip3',
+}
+
+package { 'Flask':
+  ensure   => '2.1.0',
   provider => 'pip3',
 }
 
