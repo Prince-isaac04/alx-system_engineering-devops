d1712d5153042f7c17f27bcb73b1e3fed69ee965
Prince-isaac04 <ajayioluwaseyi1692002@gmail.com>
2024-07-17 22:53:04 -0400

    update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,16 @@
 #automate task 0 but with Puppet
 
-exec {'http header':
-  	command => 'sudo apt-get update -y;
-  	sudo apt-get install nginx -y;
-  	sudo sed -i "server_name _/a add_header X-Served-By HOSTNAME;" /etc/nginx/sites-available/default
-	sudo service nginx restart',
-	provider => shell,
+exec {'update':
+  command => '/usr/bin/apt-get update',
+}
+-> package {'nginx':
+  ensure => 'present',
+}
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+}
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
